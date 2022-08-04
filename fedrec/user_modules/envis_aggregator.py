a146d178045953906e8a16c0dd5116b017895ecb
Korede <bashirkorede@gmail.com>
2022-08-04 09:53:01 +0000

    updated fedrec/user_modules/envis_aggregator.py

--- fedrec/user_modules/envis_aggregator.py
+++ fedrec/user_modules/envis_aggregator.py
@@ -4,10 +4,10 @@ from fedrec.user_modules.envis_base_module import EnvisBase
 class EnvisAggregator(EnvisBase):
     """
     EnvisAggregator is used to aggregate the results of multiple EnvisBase modules.
-    By aggregating, we mean that we can combine the results of multiple modules
-    into a single result. For example, we can aggregate the results of multiple
-    modules that are trained on the same data set. We can also aggregate the
-    results of multiple modules that are trained on different data sets.
+    By aggregating, we combine the results of multiple modules into a single result.
+    For example, we can aggregate the results of multiple modules that are trained
+    on the same data set. We can also aggregate the results of multiple modules that
+    are trained on different data sets.
 
     Methods
     -------
