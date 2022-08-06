a4464df691ee382e4485a5224750fab397350daf
Bashir Korede <bashirkorede@gmail.com>
2022-08-06 04:54:05 +0000

    Update fedrec/utilities/saver_utils.py
    Co-authored-by: Divyanshu Kumar <53843818+divyanshugit@users.noreply.github.com>

--- fedrec/utilities/saver_utils.py
+++ fedrec/utilities/saver_utils.py
@@ -37,7 +37,7 @@ class ArgsDict(dict):
 
 def create_link(original, link_name):
     """
-    This function creates a symbolic link to the original file.
+    The method creates a symbolic link to the original file.
     If the link already exists, it deletes it and creates a new
     one. This is useful for keeping the latest checkpoint.
     """
