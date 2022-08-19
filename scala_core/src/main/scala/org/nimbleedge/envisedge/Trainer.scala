9459dd8fb0e3b47fbfc06ad40e69a8504bed250f
Korede <bashirkorede@gmail.com>
2022-08-19 15:26:21 +0000

    added doc for scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala

--- scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala
+++ scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala
@@ -10,7 +10,11 @@ import akka.actor.typed.Signal
 import akka.actor.typed.PostStop
 
 object Trainer {
+    /**
+     * The behavior of the Trainer actor.
+     */
     def apply(traId: TrainerIdentifier): Behavior[Command] =
+    // Behaviors.setup is a factory for creating behaviors.
         Behaviors.setup(new Trainer(_, traId))
     
     trait Command
@@ -20,6 +24,15 @@ object Trainer {
 }
 
 class Trainer(context: ActorContext[Trainer.Command], traId: TrainerIdentifier) extends AbstractBehavior[Trainer.Command](context) {
+    /** The behavior of the Trainer actor.
+      *
+      * The Trainer actor is in charge of training the model with the given data,
+      * and then sending the trained model to the Supervisor.
+      *
+      * @param context The actor context.
+      * @param traId The identifier of the Trainer actor.
+      * @return The behavior of the Trainer actor.
+      */
     import Trainer._
 
     // TODO
@@ -28,6 +41,10 @@ class Trainer(context: ActorContext[Trainer.Command], traId: TrainerIdentifier) 
     context.log.info("Trainer {} started", traId.toString())
 
     override def onMessage(msg: Command): Behavior[Command] =
+    /** The onMessage method is responsible for handling the messages that are sent to the Trainer actor.
+      * @param msg The message that is received.
+      *
+      */
         msg match {
             // TODO
             case _ =>
@@ -35,6 +52,10 @@ class Trainer(context: ActorContext[Trainer.Command], traId: TrainerIdentifier) 
         }
     
     override def onSignal: PartialFunction[Signal,Behavior[Command]] = {
+        /** The onSignal method is responsible for handling the signals that are sent to the Trainer actor.
+          * @param signal The signal that is received.
+          * @return The behavior of the Trainer actor.
+          */
         case PostStop =>
             context.log.info("Trainer {} stopped", traId.toString())
             this
