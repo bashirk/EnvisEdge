c39fe0c8592cf5fa8b08973e0be07252b6006dc1
Korede <bashirkorede@gmail.com>
2022-08-29 12:29:00 +0000

    updated doc for scala_core/src/main/scala/org/nimbleedge/envisedge/Orchestrator.scala

--- scala_core/src/main/scala/org/nimbleedge/envisedge/Orchestrator.scala
+++ scala_core/src/main/scala/org/nimbleedge/envisedge/Orchestrator.scala
@@ -12,27 +12,27 @@ import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.Signal
 import akka.actor.typed.PostStop
 
+/** The Orchestrator is the top-level actor for the Envisedge system.
+  *
+  * It is an object of the Orchestrator class, and it is responsible for managing the lifecycle of the system.
+  * It also handles the coordination of the different actors that make up the system. Orchestration is the process
+  * of creating a system of actors that are responsible for the execution of processes and the communication between
+  * them. In this case, the orchestrator uses the actors that are responsible for the execution of the processes and
+  * the actors that are responsible for the communication between them.
+  *
+  * @param context The actor context for the orchestrator.
+  * @param config The configuration for the orchestrator.
+  * @return The behavior for the orchestrator.
+  */
 object Orchestrator {
-  /** The Orchestrator is the top-level actor for the Envisedge system.
-    *
-    * It is a state machine that is responsible for managing the lifecycle of the system, and it also handles the
-    * coordination of the different actors that make up the system. Orchestration is the process of creating a system
-    * of actors that are responsible for the execution of processes and the communication between them. In this case,
-    * the orchestrator uses the actors that are responsible for the execution of the processes and the actors that
-    * are responsible for the communication between them.
-    *
-    * @param context The actor context for the orchestrator.
-    * @param config The configuration for the orchestrator.
-    * @return The behavior for the orchestrator.
-    */
   def apply(orcId: OrchestratorIdentifier): Behavior[Command] =
     Behaviors.setup(new Orchestrator(_, orcId))
 
-  trait Command
   /** The command to start the orchestrator.
     *
     * This command is sent to the orchestrator when it is started.
     */
+  trait Command
 
   // In case any Aggregator Termination
   private final case class AggregatorTerminated(actor: ActorRef[Aggregator.Command], aggId: AggregatorIdentifier)
@@ -42,16 +42,18 @@ object Orchestrator {
   // Add messages here
 }
 
+/** The map of aggregators that are currently running.
+  *
+  * This is a map of the aggregators that are currently running. The `key` is the identifier of the aggregator, and
+  * the `value` is the actor that is responsible for the execution of the aggregator, and the actor is also responsible
+  * for the communication between the aggregator and the orchestrator.
+  *
+  * @param context The actor context for the orchestrator.
+  * @param orcId The identifier for the orchestrator.
+  * @param aggregators The map of aggregators that are currently running.
+  * @return The map of aggregators that are currently running.
+  */
 class Orchestrator(context: ActorContext[Orchestrator.Command], orcId: OrchestratorIdentifier) extends AbstractBehavior[Orchestrator.Command](context) {
-  /** The map of aggregators that are currently running.
-    *
-    * This is a map of the aggregators that are currently running. The `key` is the identifier of the aggregator, and
-    * the `value` is the actor that is responsible for the execution of the aggregator, and the actor is also responsible
-    * for the communication between the aggregator and the orchestrator.
-    *
-    * @param aggregators The map of aggregators that are currently running.
-    * @return The map of aggregators that are currently running.
-    */
   import Orchestrator._
   import FLSystemManager.{ RequestAggregator, AggregatorRegistered, RequestTrainer, RequestRealTimeGraph }
 
@@ -60,17 +62,17 @@ class Orchestrator(context: ActorContext[Orchestrator.Command], orcId: Orchestra
   var aggIdToRef : MutableMap[AggregatorIdentifier, ActorRef[Aggregator.Command]] = MutableMap.empty
   context.log.info("Orchestrator {} started", orcId.name())
   
+/** The actor that is in charge of getting the aggregator registered with the orchestrator.
+  *
+  * It determines if the aggregator is already registered, if not, it registers it. It then returns the
+  * actor reference after registration. Aditionally, if the aggregator is not in the map, then we need
+  * to request it from the FLSystemManager. This is done by sending a RequestAggregator message to the
+  * FLSystemManager.
+  *
+  * @param aggId The identifier of the aggregator.
+  * @return The actor reference of the aggregator.
+  */
   private def getAggregatorRef(aggId: AggregatorIdentifier): ActorRef[Aggregator.Command] = {
-    /** The actor that is in charge of getting the aggregator registered with the orchestrator.
-     *
-     * It determines if the aggregator is already registered, if not, it registers it. It then returns the
-     * actor reference after registration. Aditionally, if the aggregator is not in the map, then we need
-     * to request it from the FLSystemManager. This is done by sending a RequestAggregator message to the
-     * FLSystemManager.
-     *
-     * @param aggId The identifier of the aggregator.
-     * @return The actor reference of the aggregator.
-     */
     aggIdToRef.get(aggId) match {
         case Some(actorRef) =>
             actorRef
@@ -83,15 +85,15 @@ class Orchestrator(context: ActorContext[Orchestrator.Command], orcId: Orchestra
     }
   }
 
+/** The onMessage method is the main method of the orchestrator. It is responsible for handling the different
+ * commands that are sent to the orchestrator.
+ *
+ * @param msg The command that is sent to the orchestrator. It can be a RequestAggregator, RequestTrainer,
+ *            RequestRealTimeGraph, or AggregatorRegistered message. It can also be an AggregatorTerminated
+ *            message if the aggregator actor is terminated.
+ * @return The behavior of the orchestrator.
+ */
   override def onMessage(msg: Command): Behavior[Command] =
-  /** The onMessage method is the main method of the orchestrator. It is responsible for handling the different
-   * commands that are sent to the orchestrator.
-   *
-   * @param msg The command that is sent to the orchestrator. It can be a RequestAggregator, RequestTrainer,
-   *            RequestRealTimeGraph, or AggregatorRegistered message. It can also be an AggregatorTerminated
-   *            message if the aggregator actor is terminated.
-   * @return The behavior of the orchestrator.
-   */
     msg match {
       case trackMsg @ RequestAggregator(requestId, aggId, replyTo) =>
         if (aggId.getOrchestrator() != orcId) {
@@ -149,13 +151,12 @@ class Orchestrator(context: ActorContext[Orchestrator.Command], orcId: Orchestra
         // TODO
         this
     }
-  
+ /** The onSignal method is the method that is called when the orchestrator receives a signal.
+   *
+   * @param signal The signal that is sent to the orchestrator.
+   * @return The behavior of the orchestrator.
+   */
   override def onSignal: PartialFunction[Signal,Behavior[Command]] = {
-    /** The onSignal method is the method that is called when the orchestrator receives a signal.
-      *
-      * @param signal The signal that is sent to the orchestrator.
-      * @return The behavior of the orchestrator.
-      */
     case PostStop =>
       context.log.info("Orchestrator {} stopeed", orcId.toString())
       this
