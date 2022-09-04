0f50ffe7177e03f08570b432c9e2138554da8de6
Korede <bashirkorede@gmail.com>
2022-09-04 06:48:10 +0000

    updated scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala

--- scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala
+++ scala_core/src/main/scala/org/nimbleedge/envisedge/Trainer.scala
@@ -9,10 +9,10 @@ import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.Signal
 import akka.actor.typed.PostStop
 
+/**
+ * The behavior of the Trainer actor.
+ */
 object Trainer {
-    /**
-     * The behavior of the Trainer actor.
-     */
     def apply(traId: TrainerIdentifier): Behavior[Command] =
     // Behaviors.setup is a factory for creating behaviors.
         Behaviors.setup(new Trainer(_, traId))
@@ -23,16 +23,16 @@ object Trainer {
     // Add messages here
 }
 
+/** The behavior of the Trainer actor.
+ *
+ * The Trainer actor is in charge of training the model with the given data,
+ * and then sending the trained model to the Supervisor.
+ *
+ * @param context The actor context.
+ * @param traId The identifier of the Trainer actor.
+ * @return The behavior of the Trainer actor.
+ */
 class Trainer(context: ActorContext[Trainer.Command], traId: TrainerIdentifier) extends AbstractBehavior[Trainer.Command](context) {
-    /** The behavior of the Trainer actor.
-      *
-      * The Trainer actor is in charge of training the model with the given data,
-      * and then sending the trained model to the Supervisor.
-      *
-      * @param context The actor context.
-      * @param traId The identifier of the Trainer actor.
-      * @return The behavior of the Trainer actor.
-      */
     import Trainer._
 
     // TODO
@@ -40,22 +40,21 @@ class Trainer(context: ActorContext[Trainer.Command], traId: TrainerIdentifier) 
 
     context.log.info("Trainer {} started", traId.toString())
 
-    override def onMessage(msg: Command): Behavior[Command] =
     /** The onMessage method is responsible for handling the messages that are sent to the Trainer actor.
       * @param msg The message that is received.
       *
       */
+    override def onMessage(msg: Command): Behavior[Command] =
         msg match {
             // TODO
             case _ =>
                 this
         }
-    
+    /** The onSignal method is responsible for handling the signals that are sent to the Trainer actor.
+      * @param signal The signal that is received.
+      * @return The behavior of the Trainer actor.
+      */
     override def onSignal: PartialFunction[Signal,Behavior[Command]] = {
-        /** The onSignal method is responsible for handling the signals that are sent to the Trainer actor.
-          * @param signal The signal that is received.
-          * @return The behavior of the Trainer actor.
-          */
         case PostStop =>
             context.log.info("Trainer {} stopped", traId.toString())
             this
